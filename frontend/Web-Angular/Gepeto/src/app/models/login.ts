c65240fa802dd344cd2471ec99e84e48cd33a48d
Kallardian <paulobertanha03@gmail.com>
2020-10-31 17:30:46 -0300

    Added Web-Angular

---
+++ frontend/Web-Angular/Gepeto/src/app/models/login.ts
@@ -0,0 +1,9 @@
+export class Login{
+    constructor(user: string, pwd: string){
+        this.user = user;
+        this.pwd = pwd;
+    }
+
+    user: string;
+    pwd: string;
+}
---
+++ frontend/Web-Angular/Gepeto/src/app/view/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '../../../../node_modules/@angular/forms'
+import { Login } from '../../models/login';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  formLogin: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.createLoginForm();
+  }
+
+  createLoginForm() {
+    this.formLogin = this.fb.group({
+      user: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(6)
+        ])
+      ],
+      pwd: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(30)
+          
+        ])]
+    });
+  }
+
+  sendLogin() {
+    console.log(this.formLogin.value);
+  }
+
+}

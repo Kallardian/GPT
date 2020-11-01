0edfeec87bb56407e65a0463876ae8f472ec4043
Kallardian <paulobertanha03@gmail.com>
2020-10-31 22:19:42 -0300

    Classrooms ts/html added

--- frontend/Web-Angular/Gepeto/src/app/view/classrooms/classrooms.component.ts
+++ frontend/Web-Angular/Gepeto/src/app/view/classrooms/classrooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Classroom } from 'src/models/classroom.model';
 
 @Component({
@@ -6,17 +7,30 @@ import { Classroom } from 'src/models/classroom.model';
   templateUrl: './classrooms.component.html',
   styleUrls: ['./classrooms.component.css']
 })
-export class ClassroomsComponent{
+export class ClassroomsComponent implements OnInit{ 
   public classrooms: Classroom[] = [];
   public title: String = 'Salas'
+  formNumberClassrooms: FormGroup;
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
     this.classrooms.push(new Classroom(1, 'INF3AM', '2020'));
     this.classrooms.push(new Classroom(2, 'INF3BM', '2020'));
     this.classrooms.push(new Classroom(3, 'INF3CM', '2020'));
   }
 
   ngOnInit(): void {
+    this.createNumberClassroomForm()
+  }
+
+  createNumberClassroomForm() {
+    this.formNumberClassrooms = this.fb.group({
+      numberClassroom: [
+        '',
+        Validators.compose([
+          Validators.required
+        ])
+      ],
+    });
   }
 
 }

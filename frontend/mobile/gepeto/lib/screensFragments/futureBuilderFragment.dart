c0da18852d79643096e8e507e307775353bdb80c
luisfelipeluis49 <luisfelipeluis49@gmail.com>
2020-11-07 02:24:10 -0300

    added futureBuilder

---
+++ frontend/mobile/gepeto/lib/screensFragments/futureBuilderFragment.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+
+class FutureBuilderFragment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+      ),
+      body: Container(
+        child: FutureBuilder(
+          future: ,
+          builder: ,
+        )
+      ),
+      bottomNavigationBar: BottomNavigationBar(),
+    );
+  }
+}

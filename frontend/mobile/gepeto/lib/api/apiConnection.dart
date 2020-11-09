8591abcf8ff3e6fbdbb78e3479ae4c5d03cf7a16
luisfelipeluis49 <luisfelipeluis49@gmail.com>
2020-11-08 23:14:03 -0300

    it's alive!

--- frontend/mobile/gepeto/lib/api/apiConnection.dart
+++ frontend/mobile/gepeto/lib/api/apiConnection.dart
@@ -3,67 +3,73 @@ import 'package:Gepeto/api/dtos.dart';
 import 'package:http/http.dart' as http;
 
 class Conn {
-  static const baseUrl = 'http://localhost:3001/api/';
+  static const baseUrl = 'http://192.168.0.14:3001/api/';
   static const userUrl = "users/";
   static const classroomUrl = "classrooms/";
   static const groupUrl = "groups/";
   static const criterionUrl = "medium-criteria/";
   static const gradeUrl = "medium-grades/";
-
-  Future loginUser(User user) async {
+  
+  Future loginUser(User userLogin) async {
 
     var url = baseUrl + userUrl + "login";
     var response = await http.post(url, body: {
-      'ra' : user.ra,
-      'password' : user.password
+      'ra' : userLogin.ra,
+      'password' : userLogin.password
     });
 
     if (response.statusCode == 200) {
       var jsonResponse = convert.jsonDecode(response.body);
-      user.access = jsonResponse;
+      userLogin.access = jsonResponse;
     } else {
-      user.access = 0;
+      userLogin.access = 0;
     }
 
-    return user.access;
+    return userLogin.access;
   }
 
   Future getUserById(String ra) async {
 
     User user = new User();
     var url = baseUrl + userUrl + ra;
-    var response = await http.get(url);
+    Map<String, String> headers = new Map<String, String>();
+    headers["Content-type"] = "application/json";
+    headers["Accept"] = "application/json";
+    var response = await http.get(url, headers: headers);
+    var status = response.statusCode;
 
-    if (response.statusCode == 200) {
+    print(response.body);
+
+    if (status == 200) {
       var jsonResponse = convert.jsonDecode(response.body);
       user.ra = jsonResponse['ra'];
       user.fullName = jsonResponse['fullName'];
       user.password = jsonResponse['password'];
       user.access = jsonResponse['access'];
     }
-
-    return user;
+    return response.body;
   }
 
-  Future updateUser(User user) async {
+  Future updateUser(User userUpdate) async {
+
 
     var url = baseUrl + userUrl;
     var response = await http.put(url, body: {
-      "ra" : user.ra,
-      "fullName" : user.fullName,
-      "password" : user.password,
-      "access" : user.access
+      'ra' : userUpdate.ra,
+      'fullName' : userUpdate.fullName,
+      'password' : userUpdate.password,
+      'access' : userUpdate.access
     });
 
     if (response.statusCode == 200) {
       var jsonResponse = convert.jsonDecode(response.body);
-      user.ra = jsonResponse['ra'];
-      user.fullName = jsonResponse['fullName'];
-      user.password = jsonResponse['password'];
-      user.access = jsonResponse['access'];
+      userUpdate.ra = jsonResponse['ra'];
+      userUpdate.fullName = jsonResponse['fullName'];
+      userUpdate.password = jsonResponse['password'];
+      userUpdate.access = jsonResponse['access'];
     }
 
-    return user;
+    return userUpdate;
   }
 
   Future showMediumCriteria() async {
@@ -82,16 +88,16 @@ class Conn {
 
   Future showClassrooms() async {
 
-    List classrooomList = new List();
+    List classroomList = new List();
     var url = baseUrl + classroomUrl + "show";
     var response = await http.get(url);
 
     if (response.statusCode == 200) {
       var jsonResponse = convert.jsonDecode(response.body);
-      classrooomList.addAll(jsonResponse);
+      classroomList.addAll(jsonResponse);
     }
 
-    return classrooomList;
+    return classroomList;
   }
 
   Future showGroups(int idGroup) async {
@@ -108,27 +114,27 @@ class Conn {
     return groupList;
   }
 
-  Future postMediumGrade(MediumGrade mediumGrade) async {
+  Future postMediumGrade(MediumGrade mediumGradePost) async {
 
     var url = baseUrl + groupUrl;
     var response = await http.post(url, body: {
-      "idMedium": mediumGrade.idMedium,
-      "ra": mediumGrade.ra,
-      "idGroup": mediumGrade.idGroup,
-      "grade": mediumGrade.grade,
-      "attempt": mediumGrade.attempt
+      'idMedium': mediumGradePost.idMedium,
+      'ra': mediumGradePost.ra,
+      'idGroup': mediumGradePost.idGroup,
+      'grade': mediumGradePost.grade,
+      'attempt': mediumGradePost.attempt
     });
 
     if (response.statusCode == 201) {
       var jsonResponse = convert.jsonDecode(response.body);
-      mediumGrade.idMediumGrade = jsonResponse['idMediumGrade'];
-      mediumGrade.ra = jsonResponse['ra'];
-      mediumGrade.idMedium = jsonResponse['idMedium'];
-      mediumGrade.idGroup = jsonResponse['idGroup'];
-      mediumGrade.grade = jsonResponse['grade'];
-      mediumGrade.attempt = jsonResponse['attempt'];
+      mediumGradePost.idMediumGrade = jsonResponse['idMediumGrade'];
+      mediumGradePost.ra = jsonResponse['ra'];
+      mediumGradePost.idMedium = jsonResponse['idMedium'];
+      mediumGradePost.idGroup = jsonResponse['idGroup'];
+      mediumGradePost.grade = jsonResponse['grade'];
+      mediumGradePost.attempt = jsonResponse['attempt'];
     }
 
-    return mediumGrade;
+    return mediumGradePost;
   }
 }
--- frontend/mobile/gepeto/lib/screensFragments/drawer.dart
+++ frontend/mobile/gepeto/lib/screensFragments/drawer.dart
@@ -4,7 +4,36 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:Gepeto/main.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
+class User {
+  final String ra;
+  final String fullName;
+  final String password;
+  final int access;
+
+  User({this.ra, this.fullName, this.password, this.access});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      ra: json['ra'],
+      fullName: json['fullName'],
+      password: json['password'],
+      access: json['access']
+    );
+  }
+}
+
+Future<User> getUserById(String ra) async {
+  final response = await http.get('http://192.168.0.14:3001/api/users/' + ra);
+
+  if (response.statusCode == 200) {
+    return User.fromJson(convert.jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load user');
+  }
+}
 
 class DrawerFragment extends StatefulWidget {
   @override
@@ -12,6 +41,14 @@ class DrawerFragment extends StatefulWidget {
 }
 
 class _DrawerFragmentState extends State<DrawerFragment> {
+  Future<User> futureUser;
+
+  @override
+  void initState() {
+    super.initState();
+    futureUser = getUserById("123321");
+  }
+
   @override
   Widget build(BuildContext context) {
     ThemeChanger _themeChanger = Provider.of<ThemeChanger>(context);
@@ -22,22 +59,34 @@ class _DrawerFragmentState extends State<DrawerFragment> {
             DrawerHeader(
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.vertical(
-                      bottom: Radius.circular(10)),
+                      bottom: Radius.circular(10)
+                  ),
                   color: Colors.indigoAccent
               ),
 
               child: Center(
                 child: Wrap(
                   children: <Widget>[
-                    Text(
-                      'Leandro eu sou o batman Cruz',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'ShareTechMono',
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.justify,
+                    FutureBuilder<User>(
+                      future: futureUser,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return Text(
+                            snapshot.data.fullName,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'ShareTechMono',
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          textAlign: TextAlign.justify,
+                          );
+                        } else if (snapshot.hasError) {
+                          return Text("${snapshot.error}");
+                        }
+
+                        return CircularProgressIndicator();
+                      }
                     ),
                     Divider(
                       color: Colors.indigo,
